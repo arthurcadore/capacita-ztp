6fdd5e7e018ed4cbeaaaaaac92ed7f2d53194243
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-02-25 13:36:50 +0000

    :sparkles: added netstat command

--- serverDHCP/startup.sh
+++ serverDHCP/startup.sh
@@ -4,11 +4,22 @@
 
 # Script to initialize the DHCP server.
 
+# create the dhcpd.leases file if it does not exist
+touch /var/lib/dhcp/dhcpd.leases
+
+# change user of /var/lib/dhcp/dhcpd.leases to root
+chown root:root /var/lib/dhcp/dhcpd.leases
+# give permissions to the file read and write:
+chmod 664 /var/lib/dhcp/dhcpd.leases
+
 # start the DHCP server
 dhcpd -d --no-pid
 
 # verify the status of the DHCP server:
 ps aux
+
+# verify the status of the DHCP port:
+netstat -lnu
 
 # Keep the container running
 tail -f /dev/null
